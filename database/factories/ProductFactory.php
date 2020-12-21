ce702ec75e4e46e9fa7f92eada09a3e6373df486
Serhii Pylypenko <jPee.dev@gmail.com>
2020-12-21 20:19:48 +0200

    add factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $storage = array_merge($this->faker->words(2), [null]);
+
+        return [
+            'name' => $this->faker->word,
+            'model' => $this->faker->word,
+            'rating' => $this->faker->numberBetween(0, 10),
+            'view' => $this->faker->numberBetween(0, 5000),
+            'price' => $this->faker->randomFloat(2, 45, 150000),
+            'availability' => $storage[random_int(0, 2)],
+            'description' => $this->faker->text(50),
+            'photo' => $this->faker->word . hash('crc32b', $this->faker->word) . '.jpeg',
+            'brand_id' => $this->getRandomIdFrom(Brand::class),
+            'category_id' => $this->getRandomIdFrom(Category::class)
+        ];
+    }
+
+    public function getRandomIdFrom($model)
+    {
+        $ids = $model::pluck('id')->toArray();
+        $randomKey = array_rand($ids);
+
+        return $ids[$randomKey];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Brand::factory(7)->create();
+        \App\Models\Category::factory(3)->create();
+        \App\Models\Product::factory(250)->create();
     }
 }
