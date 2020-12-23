c4db1d765b938f5b8f9b47fdc4092290e04937c0
Serhii Pylypenko <jPee.dev@gmail.com>
2020-12-23 14:45:16 +0200

    display more product data

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,12 +7,16 @@
         @foreach ($products as $product)
             <section class="product mb-3 mr-3 p-3">
                 <b>id:</b> {{ $product->id }}<br />
+                <b>name:</b> {{ $product->name }}<br />
+                <b>model:</b> {{ $product->model }}<br />
+                <b>views:</b> {{ $product->view }}<br />
+                <b>rating:</b> {{ $product->rating }}<br />
                 <b>price:</b> {{ $product->price ?? 'none' }}<br />
                 <b>availability:</b> {{ $product->availability ?? 'none' }}<br />
                 <b>photo:</b> {{ $product->photo }}<br />
                 <b>category:</b> {{ $product->category->name }}<br />
-                <b>views:</b> {{ $product->view }}<br />
                 <b>brand:</b> {{ $product->brand->name }}<br />
+                <b>description:</b> {{ $product->description ?? 'none' }}<br />
                 <b>date:</b> {{ $product->updated_at }}<br />
             </section>
         @endforeach
